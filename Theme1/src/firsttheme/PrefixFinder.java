0d04a0ebc244da479a60c5ec3ec0e1730a67e22f
Serhii Yakovliuk <serhii.y.m@gmail.com>
2021-11-25 18:09:22 +0200

    Added a new solution for PrefixFinder.

--- Theme1/src/firsttheme/PrefixFinder.java
+++ Theme1/src/firsttheme/PrefixFinder.java
@@ -1,12 +1,37 @@
 package firsttheme;
 
-import java.util.Locale;
+import java.lang.reflect.Array;
 
 public class PrefixFinder {
-    public static String findAPrefix(String[] strings) {
-        if(strings.length == 0) return "";
+
+    public static String findAPrefix(String[] strings2) {
+        String[] strings = new String[strings2.length];
+        for(int i = 0; i < strings.length; i++) {
+            strings[i] = strings2[i].toLowerCase();
+        }
+        if(strings.length == 0) {
+            return "";
+        }
+        for(int i = 0; i < strings[0].length(); i++){
+            char ch = strings[0].charAt(i);
+            for(int j = 1; j < strings.length; j++) {
+                if((i == strings[j].length()) || (strings[j].charAt(i) != ch)) {
+                    return strings[0].substring(0,i);
+                }
+            }
+        }
+        return strings[0];
+    }
+
+    @Deprecated
+    public static String findAPrefixOld(String[] strings) {
+        if(strings.length == 0) {
+            return "";
+        }
         String result = strings[0].toLowerCase();
-        if (strings.length == 1) return result;
+        if (strings.length == 1) {
+            return result;
+        }
 
         for(int i = 1; i < strings.length; i++) {
             while (strings[i].toLowerCase().indexOf(result) != 0) {
@@ -16,4 +41,5 @@ public class PrefixFinder {
         }
         return result;
     }
+
 }

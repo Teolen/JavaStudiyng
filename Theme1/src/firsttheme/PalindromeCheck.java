96654eda0cc004a824ac51e019156acd320683a6
Serhii Yakovliuk <serhii.y.m@gmail.com>
2021-11-25 14:27:19 +0200

    Fixed PalindromeCheck.java

--- Theme1/src/firsttheme/PalindromeCheck.java
+++ Theme1/src/firsttheme/PalindromeCheck.java
@@ -1,38 +1,33 @@
 package firsttheme;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class PalindromeCheck {
-    private static final Pattern pattern = Pattern.compile("\\W");
-    private static Matcher matcher;
+
     public static int countOfPalindromes(String strings) {
-        int retval = 0;
-        if (strings.equals("")) return retval;
-        for(String string : strings.split(" ")) {
-            if(isPalindrome(string)) retval++;
-        }
-        return retval;
+        return countOfPalindromes(strings.split("\\W+"));
     }
+
     public static int countOfPalindromes(String[] strings) {
-        int retval = 0;
-        if(strings.length == 0) return retval;
-        for(String string : strings) {
-            if(isPalindrome(string)) retval++;
+        int result = 0;
+        if(strings.length == 0) {
+            return result;
         }
-        return retval;
+        for(String string : strings) {
+            if(!string.isEmpty()) {
+                if (isPalindrome(string)) {
+                    result++;
+                }
+            }
+        }
+        return result;
     }
+
     private static boolean isPalindrome(String str) {
-        matcher = pattern.matcher(str);
-        if (matcher.find()) return false;
         char[] chars = str.toCharArray();
         for(int i = 0; i < chars.length/2; i++) {
-            if(chars[i] != chars[chars.length-i-1]) return false;
+            if(chars[i] != chars[chars.length-i-1]) {
+                return false;
+            }
         }
         return true;
-    }
-    public static void main(String[] args) {
-        System.out.println(countOfPalindromes(args));
-        System.out.println(countOfPalindromes("abcv J4f4J . sdfv aaa 9876789"));
     }
 }

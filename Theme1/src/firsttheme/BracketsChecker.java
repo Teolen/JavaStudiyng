479d7267450c4a8d17e0f8f82436b4966d306bf9
Serhii Yakovliuk <serhii.y.m@gmail.com>
2021-11-25 21:49:42 +0200

    Updated BracketsChecker.java

--- Theme1/src/firsttheme/BracketsChecker.java
+++ Theme1/src/firsttheme/BracketsChecker.java
@@ -10,63 +10,42 @@ public class BracketsChecker {
 
     public static boolean bracketsValidation(String str) {
         Matcher matcher = pattern.matcher(str);
-        char[] charArr = str.toCharArray();
 
-        if (matcher.find()) {
-            throw new IllegalArgumentException("String contains not only brackets");
-        }
         if(str.isEmpty()) {
             throw new IllegalArgumentException("Empty string");
         }
+        if (matcher.find()) {
+            throw new IllegalArgumentException("String contains not only brackets");
+        }
+
+        char[] charArr = str.toCharArray();
+
         if(charArr.length%2 != 0) {
             return false;
         }
 
         Stack<Character> validator = new Stack<>();
 
-        for(int i = 0; i < charArr.length; i++) {
-            switch (charArr[i]) {
-                case '(':
-                case '{':
-                case '[':
-                    validator.push(charArr[i]);
-                    break;
-                case ')':
-                    if(validator.empty()) {
+        for(char ch : charArr) {
+            switch (ch) {
+                case '(', '{', '[' -> validator.push(ch);
+                case ')' -> {
+                    if (((validator.empty()) || (validator.pop() != '('))) {
                         return false;
                     }
-                    if (validator.pop() == '(') {
-                        break;
-                    } else {
+                }
+                case '}' -> {
+                    if (((validator.empty()) || (validator.pop() != '{'))) {
                         return false;
                     }
-                case '}':
-                    if(validator.empty()) {
+                }
+                case ']' -> {
+                    if (((validator.empty()) || (validator.pop() != '['))) {
                         return false;
                     }
-                    if(validator.pop() == '{') {
-                        break;
-                    } else {
-                        return false;
-                    }
-                case ']':
-                    if(validator.empty()) {
-                        return false;
-                    }
-                    if(validator.pop() == '[') {
-                        break;
-                    } else {
-                        return false;
-                    }
+                }
             }
         }
-        if(validator.empty()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public static void main(String[] args) {
-        bracketsValidation("");
+        return (validator.empty());
     }
 }

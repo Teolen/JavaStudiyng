7750aed390c8a64a313de5adc044db649b62a9cb
Serhii Yakovliuk <serhii.y.m@gmail.com>
2021-12-11 19:19:58 +0200

    rewritten solution with Streams.

--- Theme1/src/firsttheme/PrefixFinder.java
+++ Theme1/src/firsttheme/PrefixFinder.java
@@ -1,45 +1,21 @@
 package firsttheme;
 
-import java.lang.reflect.Array;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class PrefixFinder {
 
-    public static String findAPrefix(String[] strings2) {
-        String[] strings = new String[strings2.length];
-        for(int i = 0; i < strings.length; i++) {
-            strings[i] = strings2[i].toLowerCase();
-        }
+    public static String findAPrefix(String[] strings) {
         if(strings.length == 0) {
             return "";
         }
-        for(int i = 0; i < strings[0].length(); i++){
-            char ch = strings[0].charAt(i);
-            for(int j = 1; j < strings.length; j++) {
-                if((i == strings[j].length()) || (strings[j].charAt(i) != ch)) {
-                    return strings[0].substring(0,i);
-                }
-            }
-        }
-        return strings[0];
+        String[] strs = Stream.of(strings)
+                .map(String::toLowerCase)
+                .sorted()
+                .toArray(String[]::new);
+        long count = IntStream.range(0,strs[0].length())
+                .takeWhile(i ->  (strs[0].charAt(i) == strs[strs.length-1].charAt(i)))
+                .count();
+        return strs[0].substring(0,Math.toIntExact(count));
     }
-
-    @Deprecated
-    public static String findAPrefixOld(String[] strings) {
-        if(strings.length == 0) {
-            return "";
-        }
-        String result = strings[0].toLowerCase();
-        if (strings.length == 1) {
-            return result;
-        }
-
-        for(int i = 1; i < strings.length; i++) {
-            while (strings[i].toLowerCase().indexOf(result) != 0) {
-                result = result.substring(0, result.length() - 1);
-                if(result.equals("")) return result;
-            }
-        }
-        return result;
-    }
-
 }

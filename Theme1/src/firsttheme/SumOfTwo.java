702c8cc340b2d3a1c07d44c643bccf89964f3074
Serhii Yakovliuk <serhii.y.m@gmail.com>
2021-12-14 21:32:42 +0200

    rewritten solution with Streams.

--- Theme1/src/firsttheme/SumOfTwo.java
+++ Theme1/src/firsttheme/SumOfTwo.java
@@ -1,32 +1,28 @@
 package firsttheme;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 public class SumOfTwo {
 
     public static List<Integer> findASum(int[] nums, int target) {
-        List<Integer> result = new ArrayList<>();
+
         Map<Integer, Integer> numsMap = new HashMap<>();
 
-        if(nums.length == 0) {
-            throw new IllegalArgumentException("Array is empty.");
-        }
-        if((nums.length == 1) && nums[0] != target) {
+        if(nums.length < 2) {
             throw new IllegalArgumentException("Too short nums array.");
         }
 
-        for(int i = 0; i < nums.length; i++) {
-            numsMap.put(nums[i],i);
-        }
-
-        for(int i = 0; i < nums.length; i++) {
-            Integer needed = target - nums[i];
-            if((numsMap.containsKey(needed)) && ((numsMap.get(needed) != i))) {
-                result.add(i);
-                result.add(numsMap.get(needed));
-                break;
-            }
-        }
-        return result;
+        return Stream.iterate(0, i -> i < nums.length, i -> i+1)
+                .flatMap(i ->{
+                    Integer needed = target - nums[i];
+                    if((numsMap.containsKey(needed)))  {
+                        return Stream.of(numsMap.get(needed),i);
+                    }
+                    else {
+                        numsMap.put(nums[i],i);
+                        return Stream.empty();
+                    }
+                }).limit(2).toList();
     }
 }
--- Theme1/src/firsttheme/SumOfTwoTest.java
+++ Theme1/src/firsttheme/SumOfTwoTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
 
@@ -15,26 +16,22 @@ public class SumOfTwoTest {
     @Test
     public void findASumTest() {
 
-        List<List<Integer>> actual = new ArrayList<>();
         List<List<Integer>> expected = new ArrayList<>();
 
-        int[] nums1 = {2,7,11,15};
-        int target1 = 9;
+        int[][] nums = {
+                {2,7,11,15},
+                {3,2,4},
+                {3,3},
+                {5,7}
+        };
+        int[] targets = {9,6,6,6};
+
         expected.add(Arrays.asList(0,1));
-        int[] nums2 = {3,2,4};
-        int target2 = 6;
         expected.add(Arrays.asList(1,2));
-        int[] nums3 = {3,3};
-        int target3 = 6;
         expected.add(Arrays.asList(0,1));
-        int[] nums4 = {5,7};
-        int target4 = 6;
         expected.add(Arrays.asList());
 
-        actual.add(findASum(nums1,target1));
-        actual.add(findASum(nums2,target2));
-        actual.add(findASum(nums3,target3));
-        actual.add(findASum(nums4,target4));
+        List<List<Integer>> actual = Stream.of(0,1,2,3).map(i -> findASum(nums[i],targets[i])).toList();
 
         assertEquals(expected,actual);
     }
